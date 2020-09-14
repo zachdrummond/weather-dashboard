1084f8ad53933e7923d23b6602244535a4a0fccd
Zach Drummond <zachdrummond3@gmail.com>
2020-09-14 16:36:58 -0400

    Added Pseudocode

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
+// 1. App Layout
+    // Nav Bar - Weather Dashboard
+        // Black, Centered, White Header
+    // Aside (Left) - Search for a City
+        // Header
+        // Search Bar (Form) & Button
+        // Loads Bars of previously search cities
+    // Card - City's Weather for Today
+        // Left-Aligned
+        // Header - City (Date) Weather Icon
+        // Temperature
+        // Humidity
+        // Wind Speed
+        // UV Index - In a Box
+    // 5-Day Forecast
+        // Header - 5-Day Forecast:
+        // 5 Blue Boxes with White Text
+        // Month/Day/Year
+        // Weather Icon
+        // Temp: 
+        // Humidity: 
+// 2. OpenWeather API Research
+    // Section - How to Start
+    // City Search
+    // City for Today
+        // City Name, Today's Date, Weather Icon
+        // Temperature
+        // Humidity
+        // Wind Speed
+        // UV Index - In a Box
+    // 5 Day Forecast
+        // Date
+        // Weather Icon
+        // Temp:
+        // Humidity
+// 3. Functionality
+    // AJAX Call - Weather for City
+    // Add what the user searches into local storage and display below search bar
+    // UV Index Color - Favorable, Moderate, or Severe Weather Conditions
+    // User clicks previous city in search history, get from local storage and update screen
+    // When app opens, last searched city forecast displays
+
 $(document).ready(function(){
 
-
+    // Variables
     
+    // Element Variables
+
+    // Function Definitions
+
+    // Function Calls
+
+    // Event Listeners
+
+
 });
